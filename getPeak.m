de5e1ee257a648238b736f53131f6f95a3aa7521
Armaghan Sarvar <armaghansarvar@gmail.com>
2019-06-26 23:10:39 +0430

    com

--- getPeak.m
+++ getPeak.m
@@ -5,6 +5,6 @@ searchpow = abs(fft(data)/len).^2;
 [maxpeak, maxpeakindes] = max(searchpow);
 peak_freq = maxpeakindes * fs/len;
 disp(peak_freq);
-
+% we want the frequency at which the power is max
 end
 
